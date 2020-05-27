8e205e68fe1e3e740b9b08502cb28638590fa7ba
Pedro Albuquerque Santos <petersaints@gmail.com>
2020-05-27 04:37:56 +0100

    - Implemented JWK based token verification
    - Package Updates
    - Other minor things

--- calculator/config-overrides.js
+++ calculator/config-overrides.js
@@ -1,11 +1,13 @@
+const FilterWarningsPlugin = require('webpack-filter-warnings-plugin')
+
 module.exports = {
     webpack: function (config, env) {
         config.devtool = 'source-map'
-        config.module.rules.push({
-            test: /\.js$/,
-            use: ["source-map-loader"],
-            enforce: "pre"
-        })
+        config.module.rules.push({ enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' })
+        
+        config.plugins.push(new FilterWarningsPlugin({ exclude: /Failed to parse source map/ }))
+        config.stats = { warningsFilter: [/Failed to parse source map/] }
+        
         return config
     }
 }
--- calculator/src/utils/extractIdToken.js
+++ calculator/src/utils/extractIdToken.js
@@ -1,26 +1,26 @@
 import authenticationConfig from '../config/authentication'
 import queryString from 'query-string'
-import { KJUR, b64utoutf8 } from 'jsrsasign'
+import { KEYUTIL, KJUR, b64utoutf8 } from 'jsrsasign'
 
 export default async (nonce) => {
     const parameters = queryString.parse(window.location.hash);
     const idToken = parameters.id_token
     if (idToken) {
-        //console.log('ID Token', idToken)
         const idTokenSegments = idToken.split(".")
         const header = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(idTokenSegments[0]))
-        //console.log('Header', header)
         const payload = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(idTokenSegments[1]))
-        //console.log('Payload', payload)
-        const publicKey = await (await fetch(
-            `${authenticationConfig.oauth2_authentication_server}` +
-            `${authenticationConfig.oauth2_authentication_server_public_key_endpoint}`)).text()
-        //console.log('Public Key', publicKey)
-        if (payload.nonce &&
-            nonce === payload.nonce &&
-            KJUR.jws.JWS.verifyJWT(idToken, publicKey, { alg: [header.alg] })) {
-            localStorage.setItem('id_token', idToken)
-            return payload
+        if (header.jku && header.jku.startsWith(authenticationConfig.oauth2_authentication_server) && header.kid) {
+            try {
+                const json = await (await fetch(header.jku)).json()
+                const key = (json.keys || []).find(k => header.kid === k.kid)
+                if (key && payload.nonce && nonce === payload.nonce &&
+                    KJUR.jws.JWS.verifyJWT(idToken, KEYUTIL.getKey(key), { alg: [header.alg] })) {
+                    localStorage.setItem('id_token', idToken)
+                    console.log('Payload:', payload);
+                    return payload
+                }
+            } catch (e) { throw new Error('Could not extract the ID Token.') }
         }
+        return false;
     }
 }

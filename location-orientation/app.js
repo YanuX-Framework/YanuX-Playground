74f3f56b1b61a238f3dbbc1800864207b8217a27
Pedro Albuquerque Santos <petersaints@gmail.com>
2020-10-28 00:18:01 +0000

    - Finally cracked out to know if an object is facing another object.

--- location-orientation/app.js
+++ location-orientation/app.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const combinations = require('combinations');
 const { euclidean } = require('ml-distance-euclidean');
 const { dot, norm } = require('mathjs');
@@ -5,29 +6,36 @@ const { dot, norm } = require('mathjs');
 const degToRad = v => v * Math.PI / 180;
 const radToDeg = v => v * 180 / Math.PI;
 
+//NOTE: Geogebra Locations Simulation: https://www.geogebra.org/calculator/yabhuesw
 const locations = [
     {
         deviceUuid: "9ab8e750-bc1e-11e8-a769-3f2e91eebf08",
         username: "test_user_0@yanux.org",
         position: {
-            x: 1, y: 2, orientation: 0,
-            place: "/app/p37.json"
+            x: 4, y: 3,
+            orientation: -90,
+            place: "bedroom"
         }
     },
     {
         deviceUuid: "9ab8e750-bc1e-11e8-a769-3f2e91eebf09",
         username: "test_user_0@yanux.org",
         position: {
-            x: 2, y: 3, orientation: 110,
-            place: "/app/p37.json"
+            //x: 4, y: 1,
+            //x: 2, y: 3,
+            x: 4, y: 5,
+            //x: 7, y: 3,
+            orientation: 135,
+            place: "bedroom"
         }
     },
     // {
     //     deviceUuid: "9ab8e750-bc1e-11e8-a769-3f2e91eebf10",
     //     username: "test_user_0@yanux.org",
     //     position: {
-    //         x: 2, y: 3, orientation: 0,
-    //         place: "/app/p37.json"
+    //         x: 0, y: 0,
+    //         orientation: 0,
+    //         place: "bedroom"
     //     }
     // }
 ];
@@ -35,7 +43,8 @@ const locations = [
 const headingVectorFromOrientation = orientation => [Math.cos(orientation), Math.sin(orientation)];
 const computeHeadingVectors = locations => {
     locations.forEach(l => {
-        if (l.position && (l.position.orientation !== undefined || l.position.orientation !== null)) {
+        if (l.position && !_.isNil(l.position.orientation)) {
+            l.position.orientation = (360 - l.position.orientation) % 360;
             l.position.headingVector = headingVectorFromOrientation(degToRad(l.position.orientation));
         }
     });
@@ -44,15 +53,23 @@ const computeHeadingVectors = locations => {
 
 const angleBetweenVectors = (v1, v2) => Math.acos(dot(v1, v2) / (norm(v1) * norm(v2)));
 combinations(computeHeadingVectors(locations), 2, 2).forEach(([l1, l2]) => {
-    if (l1.position && l1.position.x && l1.position.y && l2.position.x && l2.position.y &&
+    if (l1.position && !_.isNil(l1.position.x) && !_.isNil(l1.position.y) && !_.isNil(l2.position.x) && !_.isNil(l2.position.y) &&
         l1.position.headingVector && l2.position.headingVector) {
+        const L1L2Vec = [l2.position.x - l1.position.x, l2.position.y - l1.position.y]
+        const L2L1Vec = [l1.position.x - l2.position.x, l1.position.y - l2.position.y]
         const distance = euclidean([l1.position.x, l1.position.y], [l2.position.x, l2.position.y])
         const dotProduct = dot(l1.position.headingVector, l2.position.headingVector);
-        const angle = radToDeg(angleBetweenVectors(l1.position.headingVector, l2.position.headingVector))
+        const headingsAngle = radToDeg(angleBetweenVectors(l1.position.headingVector, l2.position.headingVector))
+        const viewAngleL1 = radToDeg(angleBetweenVectors(l1.position.headingVector, L1L2Vec))
+        const viewAngleL2 = radToDeg(angleBetweenVectors(l2.position.headingVector, L2L1Vec))
         console.log(
-            '-------- Computed Values for L1 (' + l1.deviceUuid + ') and L2 (' + l2.deviceUuid + ') --------',
-            '\nL1 Heading Vector:', l1.position.headingVector, 'L2 Heading Vector:', l2.position.headingVector,
-            '\nL1 to L2 Distance:', distance, 'L1 dot L2:', dotProduct, 'Angle Between L1 & L2:', angle,
+            '-------- Computed Values for L1:', l1.deviceUuid, '& L2', l2.deviceUuid, '--------',
+            '\nL1(' + l1.position.x + ', ' + l1.position.y + ')', 'L2(' + l2.position.x + ', ' + l2.position.y + ')',
+            '\nOrientation L1:', l1.position.orientation, 'L2:', l2.position.orientation,
+            '\nHeading Vector L1:', l1.position.headingVector, 'L2:', l2.position.headingVector,
+            '\nVector L1 L2:', L1L2Vec, 'L2 L1:', L2L1Vec,
+            '\nDistance L1 to L2:', distance, 'L1 dot L2:', dotProduct,
+            '\nHeadings Angle:', headingsAngle, 'View Angle L1:', viewAngleL1, 'L2:', viewAngleL2,
             '\n----------------------------------------------------------------'
         );
     }

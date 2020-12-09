cc2043a2e797d46684db61e12a15eb612deb94c7
Pedro Albuquerque Santos <petersaints@gmail.com>
2020-12-09 20:08:00 +0000

    - Package Updates and a bit more experimenting

--- moving-average/app.js
+++ moving-average/app.js
@@ -1,15 +1,16 @@
 
-const period = 3;
-const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 15, 15, 15];
+const period = 10;
+const values = [2, 2, 0.5, 2, 2, 2.5, 3, 4, 2, 2.25, 1.5, 2, 3, 3.5, 4, 4, 4.5, 3.5, 4];
 const fillValues = Array(period - 1).fill(values[0]);
 
 function technicalIndicators() {
-    const { SMA } = require('technicalindicators');
-    const ma = new SMA({ period: period, values: [] });
+    console.log('[ -- Technical Indicators -- ]')
+    const { SMA: MovingAverage } = require('technicalindicators');
+    const ma = new MovingAverage({ period: period, values: [] });
 
     fillValues.forEach(fillValue => {
         const fillResult = ma.nextValue(fillValue);
-        console.log('Fill Value:', fillResult);
+        //console.log('Fill Value:', fillResult);
     });
 
     values.forEach(value => {
@@ -18,17 +19,11 @@ function technicalIndicators() {
 }
 
 function tradingSignals() {
-    const { SMA } = require('trading-signals');
-    const ma = new SMA(period);
+    console.log('[ -- Tading Signals -- ]')
+    const { SMA: MovingAverage } = require('trading-signals');
+    const ma = new MovingAverage(period);
 
-    fillValues.forEach(fillValue => {
-        ma.update(fillValue);
-        // try {
-        //     const fillResult = ma.getResult().toNumber();
-        //     console.log('Fill Value:', fillResult);
-        // } catch (e) { console.error('Fill Value Error:', e.message); }
-    });
-
+    fillValues.forEach(fillValue => { ma.update(fillValue); });
     values.forEach(value => {
         ma.update(value);
         try { console.log('Next Value:', ma.getResult().toNumber()); }
